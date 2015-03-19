8ed9992c140f71f1467a5317b5eb2e3b42b996af
sherri <sherri@sherricruz.com>
2015-03-19 13:18:21 -0700

    messing around with the button. in general, messing around

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,13 @@
 /**
  * Created by SHERRI on 3/17/15.
  */
+
+//below is the original file, tacked on the above as a test
+
 //adds routes to our app
 angular.module('myApp', ['routerRoutes', 'ngAnimate'])
+//angular.module('myApp', ['ngAnimate', 'routerRoutes', 'app.route', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
+
 
 //create the controller and inject Angular's
 // this will be the controller for the ENTIRE site
@@ -54,3 +59,15 @@ angular.module('myApp', ['routerRoutes', 'ngAnimate'])
 
         vm.message = 'Contact us';
     });
+
+
+/*adding files  to angular app -  restfultiger
+angular.module('myApp', ['ngAnimate', 'routerRoutes', 'app.route', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
+
+// application configuration to integrate token into requests
+    .config(function($httpProvider) {
+
+        // attach our auth interceptor to the http requests
+        $httpProvider.interceptors.push('AuthInterceptor');
+
+    })*/
