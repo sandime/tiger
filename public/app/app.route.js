59f24b552b69e5dac739822ada0ece9475ed3962
sherri <sherri@sherricruz.com>
2015-03-18 17:35:35 -0700

    added back in controllerAs on app.route.js

--- public/app/app.route.js
+++ public/app/app.route.js
@@ -11,36 +11,36 @@ var app = angular.module('routerRoutes', ['ngRoute'])
             // route for the home page
             .when('/', {
                 templateUrl : '/app/views/pages/home.html',
-                controller: 'homeController'
-               // controllerAs: 'home'
+                controller: 'homeController',
+                controllerAs: 'home'
             })
 
             // login page
             .when('/login', {
                 templateUrl : '/app/views/pages/login.html',
-                controller  : 'loginController'
-              //  controllerAs: 'login'
+                controller  : 'loginController',
+               controllerAs: 'login'
             })
 
             // show all users
             .when('/about', {
                 templateUrl: 'app/views/pages/about.html',
-                controller: 'aboutController'
-              //  controllerAs: 'about'
+                controller: 'aboutController',
+               controllerAs: 'about'
             })
 
             // budget
             .when('/budget', {
                 templateUrl: 'app/views/pages/budget.html',
-                controller: 'budgetController'
-            //    controllerAs: 'budget'
+                controller: 'budgetController',
+                controllerAs: 'budget'
             })
 
             // page to edit a user
             .when('/contact', {
                 templateUrl: 'app/views/pages/contact.html',
-                controller: 'contactController'
-               // controllerAs: 'contact'
+                controller: 'contactController',
+                controllerAs: 'contact'
             });
 //set app up to have pretty URLS
         $locationProvider.html5Mode(true);
