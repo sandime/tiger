4f0cb40df9d010633533ed659315fd0ad0106f0f
sherri <sherri@sherricruz.com>
2015-03-20 20:57:39 -0700

    app.js main.js budget.html need to figure how to pass the value

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,3 @@
-/**
- * Created by SHERRI on 3/17/15.
- */
-
-//below is the original file, tacked on the above as a test
 
 //adds routes to our app
 angular.module('myApp', ['routerRoutes', 'ngAnimate'])
@@ -11,12 +6,12 @@ angular.module('myApp', ['routerRoutes', 'ngAnimate'])
 
 //create the controller and inject Angular's
 // this will be the controller for the ENTIRE site
-   .controller('mainController', function() {
+    .controller('mainController', function() {
 
-  var vm = this;
+        var vm = this;
 
         // create a bigMessage variable to display in our view
-       vm.bigMessage = 'Smart money for creatives ...';
+        vm.bigMessage = 'Smart money for creatives ...';
     })
 
 // home page specific controller
@@ -43,14 +38,6 @@ angular.module('myApp', ['routerRoutes', 'ngAnimate'])
         vm.message = 'about page';
     })
 
-    // budget page specific controller
-    .controller('budgetController', function() {
-
-        var vm = this;
-
-        vm.message = 'budget form';
-    })
-
 
 // contact page controller
     .controller('contactController', function() {
@@ -58,16 +45,13 @@ angular.module('myApp', ['routerRoutes', 'ngAnimate'])
         var vm = this;
 
         vm.message = 'Contact us';
+    })
+
+    .controller('budgetController', function() {
+        // budget page specific controller
+        function budgetController($scope) {
+            $scope.$watch('rent + utilities', function (value) {
+                $scope.totalExpenses = value;
+            });
+        }
     });
-
-
-/*adding files  to angular app -  restfultiger
-angular.module('myApp', ['ngAnimate', 'routerRoutes', 'app.route', 'authService', 'mainCtrl', 'userCtrl', 'userService'])
-
-// application configuration to integrate token into requests
-    .config(function($httpProvider) {
-
-        // attach our auth interceptor to the http requests
-        $httpProvider.interceptors.push('AuthInterceptor');
-
-    })*/
